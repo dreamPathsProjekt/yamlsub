0542384de6e7128e711a4cf9496548d6a5b0203e
dreamPathsProjekt <dream.paths.projekt@gmail.com>
2018-04-15 15:53:37 +0300

    Cleanup parse_cmdline.py

--- parse_cmdline.py
+++ parse_cmdline.py
@@ -13,18 +13,4 @@ def parse_arguments():
     args = parser.parse_args()
     return args
 
-# read from:
-# export ADMIN_PASS='#/\a!()$;asd'
-# export ADMIN_PASS=http://test:8081
-
-# os.environ auto escapes env.variables
-# get_env = os.environ['ADMIN_PASS']
-
-# yaml applies single quotes around env variable with single quotes, plain value when plain value supplied
-# replacements.replace_yaml('image', 'xxx', get_env, './docker-stack-preprod.yml')
-
-# application.properties work unescaped, write 'value' as value to file
-# replacements.replace_application_properties('logging.file', 'xxx', get_env, './application.properties')
-
-# replacements.replace_ini('admin_password', 'xxx', get_env, './grafana.ini')
 
